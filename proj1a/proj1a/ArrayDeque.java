a263431ef41a5dca28be04da9e56f1fa8b32931e
zhongertiancai <50279445+zhongertiancai@users.noreply.github.com>
2021-02-13 13:21:17 +0800

    update

---
+++ proj1a/proj1a/ArrayDeque.java
@@ -0,0 +1,75 @@
+public class ArrayDeque<T> {
+    private int size;
+    private T[] items;
+    private int nextFirst;
+    private int nextLast;
+    public ArrayDeque(){
+        items =(T[])new Object[8];
+        size = 0;
+    }
+    public int size(){
+        return size;
+    }
+    private void resizing(int capacity){
+        T[] a =(T[]) new Object[capacity];
+        System.arraycopy(items ,(nextFirst + 1)%items.length  ,a, 1,items.length - nextFirst -1);
+        System.arraycopy(items , 0, a ,items.length - nextFirst +1 , (nextLast + 1)%items.length );
+        items = a;
+        nextFirst = 0;
+        nextLast = size + 1;
+    }
+    public void addFirst(T item){
+        if((nextFirst - 1)%items.length == nextLast){
+            resizing(items.length * 2);
+        }
+        items[nextFirst] = item;
+        nextFirst = (nextFirst - 1) % items.length;
+        size++;
+    }
+    public void addLast(T item){
+        if((nextLast + 1)% items.length == nextFirst){
+            resizing(items.length * 2);
+        }
+        items[nextLast] = item;
+        nextLast  = (nextLast + 1) % items.length;
+        size++;
+    }
+    public boolean isEmpty(){
+        if(size == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public void printDeque(){
+        int i = (nextFirst + 1) % items.length;
+        while(i != nextLast)
+            System.out.print(items + " ");
+            i = (i + 1)%items.length;
+        }
+    public T removeFirst(){
+        nextFirst = (nextFirst - 1) % items.length;
+        T result = items[nextFirst];
+        size--;
+        if (items.length / size>=4){
+            resizing(items.length / 2);
+        }
+        return result;
+    }
+    public T removeLast(){
+        nextLast = (nextLast + 1) % items.length;
+        T result = items[nextLast];
+        size--;
+        if (items.length / size>=4){
+            resizing(items.length / 2);
+        }
+        return result;
+    }
+    public T get(int index){
+        if (index >= size){
+            return null;
+        }else{
+            return items[(nextFirst + index + 1)% items.length];
+        }
+    }
+}
