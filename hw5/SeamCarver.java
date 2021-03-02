5bbb5bcbafefee14caad964cfe17af48abfb9469
zhongertiancai <50279445+zhongertiancai@users.noreply.github.com>
2021-03-02 21:57:27 +0800

    complete

--- hw5/SeamCarver.java
+++ hw5/SeamCarver.java
@@ -21,64 +21,104 @@ public class SeamCarver {
     public int height() {
         return height;
     }
+    private boolean v = true;
     public double energy(int x, int y) {
-        if (x < 0 || x > width - 1 || y < 0 || y > height - 1) {
-            throw new java.lang.IndexOutOfBoundsException();
-        }
-        double energy = 0;
-        int left = x - 1;
-        if (left - 1 <= 0) {
-            left += picture.width();
-        }
-        int up = y - 1;
-        if (y <= 0) {
-            up = up + picture.height();
-        }
-        int right = (x + 1) % picture.width();
-        int down = (y + 1) % picture.height();
-        Color a1 = picture.get(x, right);
-        Color a2 = picture.get(x, left);
-        energy += (a1.getBlue() - a2.getBlue()) * (a1.getBlue() - a2.getBlue());
-        energy += (a1.getGreen() - a2.getGreen()) * (a1.getGreen() - a2.getGreen());
-        energy += (a1.getRed() - a2.getRed()) * (a1.getRed() - a2.getRed());
-        a1 = picture.get(up, y);
-        a2 = picture.get(down, y);
-        energy += (a1.getBlue() - a2.getBlue()) * (a1.getBlue() - a2.getBlue());
-        energy += (a1.getGreen() - a2.getGreen()) * (a1.getGreen() - a2.getGreen());
-        energy += (a1.getRed() - a2.getRed()) * (a1.getRed() - a2.getRed());
-        return energy;
+        Color up, down, left, right;
+        up = y > 0 ? picture.get(x, y - 1) : picture.get(x, height - 1);
+        down = y < height - 1 ? picture.get(x, y + 1) : picture.get(x, 0);
+        left = x > 0 ? picture.get(x - 1, y) : picture.get(width - 1, y);
+        right = x < width - 1 ? picture.get(x + 1, y) : picture.get(0, y);
+        int rx = left.getRed() - right.getRed();
+        int gx = left.getGreen() - right.getGreen();
+        int bx = left.getBlue() - right.getBlue();
+        int ry = up.getRed() - down.getRed();
+        int gy = up.getGreen() - down.getGreen();
+        int by = up.getBlue() - down.getBlue();
+        return rx * rx + gx * gx + bx * bx + ry * ry + gy * gy + by * by;
     }
-    public int[] findHorizontalSeam() {
+    public int[] findVerticalSeam() {
         double[][] energy = new double[width][height];
         double[][] M = new double[width][height];
         for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; i++) {
+            for (int j = 0; j < height; j++) {
                 energy[i][j] = energy(i, j);
             }
         }
         int[][] prev = new int[width][height];
         for (int i = 0; i < width; i++) {
-             M[i][0] = energy[i][0];
-             prev[i][0] = i;
+            M[i][0] = energy[i][0];
+            prev[i][0] = i;
         }
-        for (int i = 0; i < width; i++) {
-            for (int j = 1; j < height; j++) {
+        for (int i = 1; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 double min = 10000000;
                 int minIndex = -1;
-                if (min > M[i][j - 1]) {
-                    min = M[i][j - 1];
-                    minIndex = i;
+                if (min > M[j][i - 1]) {
+                    min = M[j][i - 1];
+                    minIndex = j;
                 }
-                if (i != 0) {
-                    if (min > M[i - 1][j - 1]) {
-                        min = M[i - 1][j - 1];
-                        minIndex = i - 1;
+                if (j != 0) {
+                    if (min > M[j - 1][i - 1]) {
+                        min = M[j - 1][i - 1];
+                        minIndex = j - 1;
                     }
                 }
-                if (i != width - 1) {
-                    if (min > M[i + 1][j - 1]) {
-                        min = M[i + 1][j - 1];
-                        minIndex = i + 1;
+                if (j != width - 1) {
+                    if (min > M[j + 1][i - 1]) {
+                        min = M[j + 1][i - 1];
+                        minIndex = j + 1;
+                    }
+                }
+                M[j][i] = min + energy[j][i];
+                prev[j][i] = minIndex;
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < width; i++) {
+            if (M[result][height - 1] > M[i][height - 1]) {
+                result = i;
+            }
+        }
+        int[] resultA = new int[height];
+        for (int i = resultA.length - 1; i >= 0; i--) {
+            resultA[i] = result;
+            result = prev[result][i];
+        }
+        return resultA;
+    }
+
+
+    public int[] findHorizontalSeam() {
+        double[][] energy = new double[width][height];
+        double[][] M = new double[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                energy[i][j] = energy(i, j);
+            }
+        }
+        int[][] prev = new int[width][height];
+        for (int i = 0; i < height; i++) {
+            M[0][i] = energy[0][i];
+            prev[0][i] = i;
+        }
+        for (int i = 1; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                double min = 10000000;
+                int minIndex = -1;
+                if (min > M[i - 1][j]) {
+                    min = M[i - 1][j];
+                    minIndex = j;
+                }
+                if (j != 0) {
+                    if (min > M[i - 1][j - 1]) {
+                        min = M[i - 1][j - 1];
+                        minIndex = j - 1;
+                    }
+                }
+                if (j != height - 1) {
+                    if (min > M[i - 1][j + 1]) {
+                        min = M[i - 1][j + 1];
+                        minIndex = j + 1;
                     }
                 }
                 M[i][j] = min + energy[i][j];
@@ -86,32 +126,30 @@ public class SeamCarver {
             }
         }
         int result = 0;
-        for (int i = 0; i < width; i++) {
-            if (M[height - 1][result] > M[height - 1][i]) {
+        for (int i = 0; i < height; i++) {
+            if (M[width - 1][result] > M[width - 1][i]) {
                 result = i;
             }
         }
         int[] resultA = new int[width];
         for (int i = resultA.length - 1; i >= 0; i--) {
             resultA[i] = result;
-            result = prev[result][i];
+            result = prev[i][result];
         }
         return resultA;
     }
-    public int[] findVerticalSeam() {
-        int temp = width;
-        width = height;
-        height = temp;
-        int[] resultV = findHorizontalSeam();
-        temp = height;
-        height = width;
-        width = temp;
-        return resultV;
-    }
     public void removeHorizontalSeam(int[] seam) {
-        return;
+        if (seam.length != width) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        picture = SeamRemover.removeHorizontalSeam(picture, seam);
+        height--;
     }
     public void removeVerticalSeam(int[] seam) {
-        return;
+        if (seam.length != height) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        picture = SeamRemover.removeVerticalSeam(picture, seam);
+        width--;
     }
 }
